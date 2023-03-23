a980de86066811dd39b80926ce34718af719fd0d
Maxim Manylov <manylov@gmail.com>
2023-03-23 16:31:34 +0600

    hardhat deploy, imports files from npm

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { getContractsPathsFromDeps, node_url } from "./utils/config-helpers";
+import "hardhat-dependency-compiler";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100000,
+      },
+    },
+  },
+  paths: {
+    sources: "contracts",
+  },
+  dependencyCompiler: {
+    paths: getContractsPathsFromDeps("@gearbox-protocol/core-v2/contracts/")
+  },
+  networks: {
+    hardhat: {
+      forking: process.env.HARDHAT_FORK
+        ? {
+            enabled: true,
+            url: node_url(process.env.HARDHAT_FORK),
+          }
+        : undefined,
+    },
+  },
 };
 
 export default config;
